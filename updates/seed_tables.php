d5bb9cc01fb235455f44ba3d5c898a6af41ea652
charles <charles.stolive@dayonmars.fr>
2020-02-07 10:24:49 +0100

    reactivation seeds

--- updates/seed_tables.php
+++ updates/seed_tables.php
@@ -6,8 +6,8 @@ class SeedTables extends Seeder
 {
     public function run()
     {
-        // Db::table('waka_wcms_needs')->truncate();
-        // Excel::import(new \Waka\Wcms\Classes\Imports\NeedImport, plugins_path('waka/wcms/updates/excels/start_needs.xlsx'));
+        Db::table('waka_wcms_needs')->truncate();
+        Excel::import(new \Waka\Wcms\Classes\Imports\NeedImport, plugins_path('waka/wcms/updates/excels/start_needs.xlsx'));
     }
 
 }

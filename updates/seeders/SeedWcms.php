704a147d9184821d029a33bb1dc91e55866b2a48
charles <charles.stolive@dayonmars.fr>
2020-02-15 15:29:49 +0100

    Iversion du seed solution et need

--- updates/seeders/SeedWcms.php
+++ updates/seeders/SeedWcms.php
@@ -8,14 +8,13 @@ class SeedWcms extends Seeder
 {
     public function run()
     {
+        echo '--Solution Import' . PHP_EOL;
+        Db::table('waka_wcms_solutions')->truncate();
+        Excel::import(new \Waka\Wcms\Classes\Imports\SolutionImport, plugins_path('waka/wcms/updates/excels/start_solutions.xlsx'));
 
         echo '--Need Import' . PHP_EOL;
         Db::table('waka_wcms_needs')->truncate();
         Excel::import(new \Waka\Wcms\Classes\Imports\NeedImport, plugins_path('waka/wcms/updates/excels/start_needs.xlsx'));
-
-        echo '--Solution Import' . PHP_EOL;
-        Db::table('waka_wcms_solutions')->truncate();
-        Excel::import(new \Waka\Wcms\Classes\Imports\SolutionImport, plugins_path('waka/wcms/updates/excels/start_solutions.xlsx'));
 
     }
 

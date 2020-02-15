6a4ad0dc18994293a814da91b54301a637ff7d83
charles <charles.stolive@dayonmars.fr>
2020-02-15 11:42:19 +0100

    correction seeders

--- updates/seeders/SeedWcms.php
+++ updates/seeders/SeedWcms.php
@@ -1,16 +1,19 @@
-<?php namespace Waka\Wcms\Updates;
+<?php namespace Waka\Wcms\Updates\Seeders;
 
-use Db;
+use DB;
 use Excel;
 use Seeder;
 
-class SeedTables extends Seeder
+class SeedWcms extends Seeder
 {
     public function run()
     {
+
+        echo '--Need Import' . PHP_EOL;
         Db::table('waka_wcms_needs')->truncate();
         Excel::import(new \Waka\Wcms\Classes\Imports\NeedImport, plugins_path('waka/wcms/updates/excels/start_needs.xlsx'));
 
+        echo '--Solution Import' . PHP_EOL;
         Db::table('waka_wcms_solutions')->truncate();
         Excel::import(new \Waka\Wcms\Classes\Imports\SolutionImport, plugins_path('waka/wcms/updates/excels/start_solutions.xlsx'));
 

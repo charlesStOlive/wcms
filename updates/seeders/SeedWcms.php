856c1ef54d6831d3c65d5375cadf844bc98930f2
charles <charles.stolive@dayonmars.fr>
2020-02-20 09:00:48 +0100

    correction seeder

--- updates/seeders/SeedWcms.php
+++ updates/seeders/SeedWcms.php
@@ -8,6 +8,8 @@ class SeedWcms extends Seeder
 {
     public function run()
     {
+        Db::table('waka_wcms_needs_solutions')->truncate();
+
         echo '--Solution Import' . PHP_EOL;
         Db::table('waka_wcms_solutions')->truncate();
         Excel::import(new \Waka\Wcms\Classes\Imports\SolutionImport, plugins_path('waka/wcms/updates/excels/start_solutions.xlsx'));
